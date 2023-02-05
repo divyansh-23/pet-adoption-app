251c1513b6bd170609d16917ac25f8d3ae4335f1
divyansh-23 <divyansh.s230@gmail.com>
2023-02-05 05:23:32 -0800

    Added a hook in SearchParams component.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  //this is a hook. Generally, when you see the word hook, you are most probably dealing with a hook.
+  //by convention, all hookes start with "use"
+  const [location, setLocation] = useState("");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
         </label>
         <button>Submit</button>
       </form>
