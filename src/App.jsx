2318e9c4817c87308df52fd4d3489cce40093683
divyansh-23 <divyansh.s230@gmail.com>
2023-02-06 01:30:23 -0800

    Added React Router and useParams hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { createRoot } from 'react-dom/client';
-import SearchParams from './SearchParams';
+import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>)
+root.render(<App />);
