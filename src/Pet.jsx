97d9da1f589b071a1498be383b462ae74560f9d7
divyansh-23 <divyansh.s230@gmail.com>
2023-02-06 00:00:24 -0800

    Dried out the code by creating more components and styled the PET component

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,20 +1,21 @@
-//this is the old way of writing the same thing which is written later
+const Pet = ({animal, breed, name, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
 
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("h2", {}, props.animal),
-//     React.createElement("h2", {}, props.breed),
-//   ]);
-// };
-
-const Pet = (props) => {
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} -- {breed} -- {location}
+        </h2>
+      </div>
+    </a>
   );
 };
 
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
+import Results from "./Results";
 import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -81,14 +81,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
